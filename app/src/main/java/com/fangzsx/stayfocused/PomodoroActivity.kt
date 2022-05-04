8fb805d7f66238a8a97097bbbe81520968306a3d
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-04 11:38:52 +0800

    drag imageview to different layouts

--- app/src/main/java/com/fangzsx/stayfocused/PomodoroActivity.kt
+++ app/src/main/java/com/fangzsx/stayfocused/PomodoroActivity.kt
@@ -4,7 +4,9 @@ import android.content.ClipData
 import android.content.ClipDescription
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.DragEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.fangzsx.stayfocused.databinding.ActivityPomodoroBinding
@@ -20,6 +22,7 @@ class PomodoroActivity : AppCompatActivity() {
 
         binding.ivTimer.setOnLongClickListener {
 
+            //save current parent position using tags
             val parent = it.parent as LinearLayout
             val timerParentName = parent.tag.toString()
             val item = ClipData.Item(timerParentName)
@@ -31,8 +34,51 @@ class PomodoroActivity : AppCompatActivity() {
 
             it.visibility = View.INVISIBLE
             true
-
         }
+
+        val dragListener = View.OnDragListener { view, event ->
+
+            when(event.action){
+                DragEvent.ACTION_DRAG_STARTED -> {
+                    event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                }
+                DragEvent.ACTION_DRAG_ENTERED -> {
+                    view.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DRAG_LOCATION -> true
+                DragEvent.ACTION_DRAG_EXITED -> {
+                    view.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DROP -> {
+                    val item = event.clipData.getItemAt(0)
+                    val fromParentName = item.text
+                    view.invalidate()
+
+                    val dragView = event.localState as View
+                    val owner = dragView.parent as ViewGroup
+                    owner.removeView(dragView)
+
+                    val destination = view as LinearLayout
+                    destination.addView(dragView)
+                    dragView.visibility = View.VISIBLE
+                    true
+                }
+
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    view.invalidate()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.llTop.setOnDragListener(dragListener)
+        binding.llBottom.setOnDragListener(dragListener)
+        binding.llLeft.setOnDragListener(dragListener)
+        binding.llCenter.setOnDragListener(dragListener)
+        binding.llRight.setOnDragListener(dragListener)
 
 
 
