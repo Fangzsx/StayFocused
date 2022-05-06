56d6b6ba69819f94006f55c4e471881d6ae639a5
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-06 12:53:49 +0800

    add checkbox group

--- app/src/main/java/com/fangzsx/stayfocused/SettingsActivity.kt
+++ app/src/main/java/com/fangzsx/stayfocused/SettingsActivity.kt
@@ -2,10 +2,43 @@ package com.fangzsx.stayfocused
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.Toast
+import com.fangzsx.stayfocused.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val checkBoxGroup = mutableListOf<CheckBox>().apply {
+            addAll(
+                arrayOf(
+                    binding.cbPalette1,
+                    binding.cbPalette2,
+                    binding.cbPalette3,
+                    binding.cbPalette4,
+                )
+            )
+        }
+        
+
+        for (checkbox in checkBoxGroup){
+            checkbox.setOnCheckedChangeListener { _, isChecked ->
+                if(isChecked){
+                    checkBoxGroup.filter { it.id != checkbox.id }.forEach { other ->
+                        other.isChecked = false
+                    }
+                }
+            }
+        }
+
+
+
+
     }
 }
